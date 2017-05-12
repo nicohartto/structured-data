a1a556515b5b32116abcc61e0116e7148ca7678e
Nico Hartto <nico.hartto@solita.fi>
2017-05-12 17:07:53 +0300

    Progress until Exercise 15

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,17 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [thing (+ x x)]
+    (Math/pow thing thing)))
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2)))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[first second third]]
+  (+ first third))
 
 (defn point [x y]
   [x y])
@@ -18,38 +19,54 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn width [[x1 y1] [x2 y2]]
+  (- x2 x1))
 
-(defn height [rectangle]
-  :-)
+(defn height [[x1 y1] [x2 y2]]
+  (- y2 y1))
 
 (defn square? [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (if (= (width [x1 y1] [x2 y2]) (height [x1 y1] [x2 y2]))
+      true
+      false)))
 
 (defn area [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (* (width [x1 y1] [x2 y2]) (height [x1 y1] [x2 y2]))))
+
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle [x y] point]
+    (if (and (<= x1 x x2) (<= y1 y y2))
+      true
+      false)))
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[x y] inner]
+    (if
+      (and (contains-point? outer x) (contains-point? outer y))
+      true
+      false)))
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 (defn multiple-authors? [book]
-  :-)
+  (if (> (author-count book) 1)
+    true
+    false))
 
 (defn add-author [book new-author]
-  :-)
+  (assoc book :authors (conj (:authors book) new-author)))
 
 (defn alive? [author]
-  :-)
+  (if (:death-year author)
+    false
+    true))
 
 (defn element-lengths [collection]
   :-)
